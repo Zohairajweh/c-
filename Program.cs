835e19abddfe60907cd67221171466dd3c715226
zohair <zohair@hotmail.com>
2020-02-12 11:38:06 -0700

    first c# project

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace c_review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //     int x =30;
+         
+        //    // Console.WriteLine("Hello World!"+x);
+        //     if (x<30){
+
+        //       Console.WriteLine("x is less than therty");
+        //     }
+        //       else if (x==30)
+        //       {              Console.WriteLine("X is thirty");
+
+        //       }     
+        //           else
+        //           {
+        //            Console.WriteLine("x is greater than therty");
+
+        //           }
+              
+int a = 3;
+switch(a){
+
+    case 1 :
+         Console.WriteLine("value is 1!");
+break;
+    case 2 :
+         Console.WriteLine("value is 2!");
+break;
+    case 3 :
+         Console.WriteLine("value is 3!");
+break;
+
+default :
+break;
+}
+
+
+        }
+    }
+}
